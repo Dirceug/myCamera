19d8d6d09141dc80e4bcba84a1ca608d36b517d6
Dirceu Garcia <70041892+Dirceug@users.noreply.github.com>
2023-07-24 18:02:03 -0300

    Iniciando a utilização da Câmera

---
+++ App.js
@@ -0,0 +1,47 @@
+import React, {useState, useEffect} from 'react'
+import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
+
+import { Camera } from 'expo-camera'
+
+export default function App() {
+
+  const [type, setType] = useState(Camera.Constants.Type.back)
+  const [hasPermission, setHasPermission] = useState(null)
+
+  useEffect (() =>{
+    (async () => {
+      const { status } = await Camera.requestPermissionAsync()
+      setHasPermission(status === "granted")
+    })()
+  }, [])
+
+  if(hasPermission === null){
+    return</View>
+  }
+
+  if(hasPermission === false){
+    return <Text>Acesso Negado</Text>
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Camera
+        style={styles.camera}
+        type={type}      
+      >
+
+      </Camera>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  camera: {
+    widht: '100%',
+    height: '100%'
+  }
+});
